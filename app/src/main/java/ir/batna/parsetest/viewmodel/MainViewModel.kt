d8c2536e5287b8fe9d75c44276ccc660457fec5f
alireza_1082 <rahmani@batna.ir>
2023-09-13 11:12:03 +0330

    Fix MainViewModel data

--- app/src/main/java/ir/batna/parsetest/viewmodel/MainViewModel.kt
+++ app/src/main/java/ir/batna/parsetest/viewmodel/MainViewModel.kt
@@ -2,23 +2,20 @@ package ir.batna.parsetest.viewmodel
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.parse.FindCallback
 import com.parse.ParseObject
-import com.parse.ParseQuery
 import ir.batna.parsetest.MainActivity
 import ir.batna.parsetest.api.ParseServer
 import ir.batna.parsetest.model.Request
 import ir.batna.parsetest.model.RequestRepository
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainViewModel(parseServer: ParseServer, mainActivity: MainActivity) : ViewModel() {
 
     private val requestRepository = RequestRepository()
     private val _requestData = MutableStateFlow(emptyList<Request>())
-    val requestData: StateFlow<List<Request>> = _requestData
+    val requestData = _requestData.asStateFlow()
 
     private var parseServerObject: ParseServer
 
